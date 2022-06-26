c41cb0dbafe734a609f7c555013093e9ea21cecc
vkutovoi <sir.kutovoi@yandex.ru>
2022-06-26 14:09:10 +0300

    src: add recv impl to sockiface class

--- src/network/sockiface.hpp
+++ src/network/sockiface.hpp
@@ -20,7 +20,7 @@ class Sockiface : public Netiface<int> {
     cl_status InitClient();
     cl_status InitServer();
     cl_status Send(int sock, std::string msg, CmdType cmd_type);
-    cl_status Recv(int sock);
+    cl_status Recv(int sock, std::string *msg, CmdType *cmd);
 };
 
 Sockiface::Sockiface(char *addr, int port, sa_family_t sin_family) {
@@ -141,6 +141,43 @@ fn_fail:
     goto fn_exit;
 }
 
+cl_status Sockiface::SockRead(int sock, const void *recv_buf, int maxlen) {
+    int recvd = 0, tmp = 0;
+
+    cl_status status = cl_status::SUCCESS;
+    while (1) {
+        do {
+            tmp = read(sock, (char *)recv_buf + recvd, maxlen - recvd);
+            printf("tmp=%d, errno=%d\n", tmp, errno);
+            if (tmp < 0) {
+                if (errno == ECONNRESET) {
+                    ERROR_PRINT("ECONNRESET\n");
+                    status = cl_status::ERROR;
+                    goto fn_exit;
+                }
+            }
+        } while (tmp < 0 && errno == EINTR);
+
+        if (tmp < 0) {
+            status = cl_status::ERROR;
+            ERROR_PRINT("read error\n");
+            goto fn_exit;
+        } else if (tmp == 0) {
+            goto fn_exit;
+        } else {
+            recvd += tmp;
+        }
+
+        if (recvd == maxlen) break;
+    }
+
+fn_exit:
+    return status;
+
+fn_fail:
+    goto fn_exit;
+}
+
 cl_status Sockiface::Send(int sock, std::string msg, CmdType cmd_type) {
     // step 1: sending command
     struct Cmd cmd;
@@ -149,7 +186,7 @@ cl_status Sockiface::Send(int sock, std::string msg, CmdType cmd_type) {
     cmd.msg_size = msg.size();
     cmd.type = cmd_type;
 
-    if (SockWrite(this->id, &cmd, sizeof(cmd)) == cl_status::ERROR) {
+    if (SockWrite(sock, &cmd, sizeof(cmd)) == cl_status::ERROR) {
         ERROR_PRINT("send failed\n");
         return cl_status::ERROR;
     }
@@ -159,7 +196,7 @@ cl_status Sockiface::Send(int sock, std::string msg, CmdType cmd_type) {
         // step 2: sending actual data
         buf = (char *)malloc(sizeof(char) * msg.size());
         strcpy(buf, msg.c_str());
-        if (SockWrite(this->id, buf, msg.size()) == cl_status::ERROR) {
+        if (SockWrite(sock, buf, msg.size()) == cl_status::ERROR) {
             free(buf);
             ERROR_PRINT("send failed\n");
 
@@ -173,6 +210,35 @@ cl_status Sockiface::Send(int sock, std::string msg, CmdType cmd_type) {
     return cl_status::SUCCESS;
 }
 
-cl_status Sockiface::Recv(int sock) { return cl_status::SUCCESS; }
+cl_status Sockiface::Recv(int sock, std::string *msg, CmdType *type) {
+    // step 1: recv command
+    Cmd cmd;
+    char *buf = NULL;
+
+    if (SockRead(sock, &cmd, sizeof(cmd)) == cl_status::ERROR) {
+        ERROR_PRINT("recv cmd failed\n");
+        return cl_status::ERROR;
+    }
+
+    *type = cmd.type;
+
+    // step 2: recv msg
+
+    if (cmd.msg_size > 0) {
+        DEBUG_PRINT("waiting for msg_size=%d\n", cmd.msg_size);
+        buf = (char *)malloc(cmd.msg_size * sizeof(char));
+        memset(buf, 0, cmd.msg_size * sizeof(char));
+
+        if (SockRead(sock, buf, cmd.msg_size) == cl_status::ERROR) {
+            ERROR_PRINT("recv msg failed\n");
+            return cl_status::ERROR;
+        }
+        *msg = std::string(buf);
+        free(buf);
+        buf = NULL;
+    }
+
+    return cl_status::SUCCESS;
+}
 
 #endif /* !SOCKIFACE_H */
